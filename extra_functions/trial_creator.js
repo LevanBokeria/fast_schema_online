6dc0bb34c16164015e00147586a0727151565108
levan <levan.bokeria@gmail.com>
2021-05-04 11:32:25 +0400

    adjusted the trial_creator.js. Now it goes down the column

--- extra_functions/trial_creator.js
+++ extra_functions/trial_creator.js
@@ -1,49 +1,68 @@
-function trial_creator(stimuli){
+function trial_creator(all_conditions){
 
     let all_trials = []
     let n_trials_per_pa  = jatos.studySessionData.inputData.n_trials_per_pa
-    let n_trials_per_ses = n_trials_per_pa * stimuli.consistent_schema.length
+    // let n_trials_per_ses = n_trials_per_pa * stimuli.consistent_schema.length
 
 
-    for (iSes=0; iSes<jatos.studySessionData.inputData.n_schema_ses_per_cond; iSes++){
+    // for (iSes=0; iSes<jatos.studySessionData.inputData.n_schema_ses_per_cond; iSes++){
 
-        Object.keys(stimuli).forEach(function(curr_condition,index){
-            // debugger
-            let curr_stimuli = stimuli[curr_condition]
-            let allpas    = []
-            let allpaidxs = []
+        Object.keys(all_conditions).forEach(function(iCond,index){
+            debugger
+            let iCondStages = all_conditions[iCond]
 
-            for (iSt=0; iSt<curr_stimuli.length; iSt++){
+            Object.keys(iCondStages).forEach(function(stage,counter){
 
-                let pa_stimuli = Array(n_trials_per_pa).fill(curr_stimuli[iSt])
-                let pa_idxs    = Array(n_trials_per_pa).fill((iSt+1))
+                let iStageStimuli = iCondStages[stage]
 
-                allpas.push(...pa_stimuli)
-                allpaidxs.push(...pa_idxs)
+                    let n_trials_per_ses = n_trials_per_pa * iStageStimuli.length
+                    let istage_n_ses = []
 
-            }
-          
-            let session_trials = []
+                    if (stage == 'schema_stimuli'){
+                        istage_n_ses = jatos.studySessionData.inputData.n_schema_ses_per_cond
+                    } else {
+                        istage_n_ses = jatos.studySessionData.inputData.n_new_pa_ses_per_cond                       
+                    }
 
-            for (i=0; i < n_trials_per_ses; i++){
+                    for (iSes=0; iSes<istage_n_ses; iSes++){
 
-                session_trials[i] = {
-                    color: jatos.studySessionData.inputData.condition_colors[curr_condition],
-                    img_path: allpas[i],
-                    stimulus_idx: allpaidxs[i],
-                    condition: curr_condition,
-                }
-            }
+                        let allpas    = []
+                        let allpaidxs = []
 
-            // randomize the order of the trials 
-            // debugger
-            session_trials = 
-            jsPsych.randomization.shuffleNoRepeats(session_trials,function(a,b){return a.img_path === b.img_path})
+                        for (iSt=0; iSt<iStageStimuli.length; iSt++){
+                            let pa_stimuli = Array(n_trials_per_pa).fill(iStageStimuli[iSt])
+                            let pa_idxs    = Array(n_trials_per_pa).fill((iSt+1))
 
-            all_trials.push(session_trials)
+                            allpas.push(...pa_stimuli)
+                            allpaidxs.push(...pa_idxs)
+                        }
+                        let session_trials = []
+                        for (i=0; i < n_trials_per_ses; i++){
+
+                            session_trials[i] = {
+                                color: jatos.studySessionData.inputData.condition_colors[iCond],
+                                img_path: allpas[i],
+                                stimulus_idx: allpaidxs[i],
+                                condition: iCond,
+                                stage: stage,
+                            }
+                        }
+                        // randomize the order of the trials 
+                        // debugger
+                        session_trials = 
+                        jsPsych.randomization.shuffleNoRepeats(session_trials,function(a,b){return a.img_path === b.img_path})
+
+                        all_trials.push(session_trials)
+                    }
+            })
+
+
+
+
+
 
         });
-    }
+    // }
 
     return all_trials
 }
